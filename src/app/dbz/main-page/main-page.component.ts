977096889c830bed644c1e2f0ad300db08b67902
Enrique <enrique@deltasoftinformatica.com>
2023-02-07 18:37:52 +0100

    feat:fin

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { Personaje } from '../interfaces/dbz.interface';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+})
+export class MainPageComponent {
+
+  nuevo: Personaje = {
+    nombre: 'Maestro',
+    poder: 5000
+  }
+
+  constructor() {}
+
+}
